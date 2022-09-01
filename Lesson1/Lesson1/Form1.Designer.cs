326c06ba4ff13e06c2c03a5ba66e214b85fbf042
Roma_xs97 <Roma_xs97@student.itstep.org>
2022-09-01 17:22:53 +0300

    ButtonMaker

--- Lesson1/Lesson1/Form1.Designer.cs
+++ Lesson1/Lesson1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(332, 187);
+            this.button1.Location = new System.Drawing.Point(59, 46);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(73, 45);
             this.button1.TabIndex = 0;
@@ -41,7 +41,7 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
-            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseEnter);
             // 
             // Form1
             // 
@@ -52,6 +52,10 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.button1_Click);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
--- Lesson1/Lesson1/Form1.cs
+++ Lesson1/Lesson1/Form1.cs
@@ -21,27 +21,84 @@ namespace Lesson1
         {
 
         }
-        private int counter; 
+        private Int32 counter; 
         private void button1_Click(object sender, EventArgs e)
         {
+            Button b = sender as Button;
             counter++;
             button1.Text = counter.ToString(); 
-            Random rnd = new Random(); 
-            int numX = rnd.Next(700);
-            int numY = rnd.Next(400);
+            Random rnd = new Random();
+            Int32 numX = rnd.Next(300);
+            Int32 numY = rnd.Next(100);
             button1.Location = new Point(numX, numY);
         }
 
         private void button1_MouseEnter(object sender, EventArgs e)
         {
-        Button b =sender as Button;
+            Button b = sender as Button;
             button1.BackColor = Color.Yellow;
         }
-
-        private void button1_MouseLeave(object sender, EventArgs e)
+        private Int32 counter2;
+        Int32 X, Y, X2, Y2;
+        Button MakeButton;
+        Boolean buttonReleased = true;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            Button b = sender as Button;
-            button1.BackColor = Color.Blue; 
+            if (!buttonReleased)
+            {
+
+                if (MakeButton != null)
+                {
+                    var newLocationX = 0;
+                    var newLocationY = 0;
+                    if (e.X - X2 > 0)
+                    {
+                        X = e.X - X2;
+                        newLocationX = X2;
+                    }
+                    else
+                    {
+                        newLocationX = e.X;
+                        X = Math.Abs(X2 - e.X);
+                    }
+                    if (e.Y - Y2 > 0)
+                    {
+                        Y = e.Y - Y2;
+                        newLocationY = Y2;
+                    }
+                    else
+                    {
+                        newLocationY = e.Y;
+                        Y = Math.Abs(Y2 - e.Y);
+                    }
+                    MakeButton.Size = new Size(X, Y);
+                    MakeButton.Location = new Point(newLocationX, newLocationY);
+                }
+            }
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            X = 0;
+            Y = 0;
+            X2 = e.X;
+            Y2 = e.Y;
+
+            buttonReleased = false;
+            MakeButton = new Button
+            {
+                Location = new Point(X2, Y2),
+                Size = new Size(X, Y)
+            };
+            this.Controls.Add(MakeButton);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            buttonReleased = true;
+            counter2++;
+            MakeButton.Text = counter2.ToString();
         }
     }
 }
