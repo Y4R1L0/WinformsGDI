1ce8630e5d8b62e0de8899657a4b70675b0517c2
Roma_xs97 <Roma_xs97@student.itstep.org>
2022-09-02 14:35:07 +0300

    Buttons

--- Lesson2_Buttons/Lesson2_Buttons/Form1.cs
+++ Lesson2_Buttons/Lesson2_Buttons/Form1.cs
@@ -15,6 +15,35 @@ namespace Lesson2_Buttons
         public Form1()
         {
             InitializeComponent();
+            CreateButtons();
+        } 
+        Random random = new Random();
+        Int32 M = 5;
+        Int32 N = 5;
+        Int32 x = 50;
+        Int32 y = 10;
+
+        private void CreateButtons()
+        {
+            for (Int32 i = 0; i < M; i++)
+            {
+                for (Int32 j = 0; j < N; j++)
+                {
+
+                    var button = new Button();
+                    button.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                    button.Location = new Point(i * x + i * y, j * x + j * y);
+                    button.Size = new Size(x, x);
+                    button.Text = ((i + 1) * (j + 1)).ToString();
+                    button.Click += new EventHandler(ButtonRemove);
+                    this.Controls.Add(button);
+                }
+            }
         }
+        private void ButtonRemove(object sender, EventArgs e)
+        {
+            Controls.Remove((Button)sender);
+        }
+
     }
 }
