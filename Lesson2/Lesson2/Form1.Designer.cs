137bfaab94fdfd06d687a687563a107465cf9f31
Roma_xs97 <Roma_xs97@student.itstep.org>
2022-09-02 13:16:32 +0300

    First and second practice

---
+++ Lesson2/Lesson2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Lesson2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelMilliesec = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(35, 47);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(125, 37);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "label1";
+            // 
+            // labelMilliesec
+            // 
+            this.labelMilliesec.AutoSize = true;
+            this.labelMilliesec.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMilliesec.Location = new System.Drawing.Point(35, 105);
+            this.labelMilliesec.Name = "labelMilliesec";
+            this.labelMilliesec.Size = new System.Drawing.Size(125, 37);
+            this.labelMilliesec.TabIndex = 1;
+            this.labelMilliesec.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelMilliesec);
+            this.Controls.Add(this.labelTime);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelMilliesec;
+    }
+}
+
---
+++ Lesson2/Lesson2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson2
+{
+    public partial class Form1 : Form
+    {
+        private static Timer vTimer=new Timer();
+        private void ShowTime(object vObj,EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToLongTimeString();
+            labelMilliesec.Text = DateTime.Now.ToString("ffff");
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            labelTime.Text = DateTime.Now.ToLongTimeString();
+            labelMilliesec.Text = DateTime.Now.ToString("ffff");
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        { 
+            Point? newLocation = null;
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                newLocation = new Point(this.Location.X, this.Location.Y + 25);
+            }
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                newLocation = new Point(this.Location.X - 25, this.Location.Y);
+            }
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                newLocation = new Point(this.Location.X, this.Location.Y - 25);
+            }
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                newLocation = new Point(this.Location.X + 25, this.Location.Y);
+            }
+            if (newLocation != null)
+            {
+                if (Screen.PrimaryScreen.Bounds.Contains(new Rectangle(newLocation.Value, this.Size)))
+                {
+                    this.Location = newLocation.Value;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToString("hh.mm.ss.fff");
+            labelMilliesec.Text = DateTime.Now.ToString("ffff");
+        }
+    } 
+}
