9b53ba0fa4f2bcd874265d74663ff8e0dfd0b2cf
Roma_xs97 <Roma_xs97@student.itstep.org>
2022-09-12 15:49:15 +0300

    DZ 07_09_2022

---
+++ InfiniteMenu/InfiniteMenu/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfiniteMenu
+{
+    public partial class Form1 : Form
+    {
+        int stripCounter = 1;
+
+        public Form1()
+        {
+            InitializeComponent();  
+            var newItem = new ToolStripMenuItem();
+            newItem.Name = stripCounter.ToString();
+            newItem.Text = stripCounter.ToString();
+            newItem.MouseHover += MHoover;
+            toolStripMenuItem1.DropDownItems.Add(newItem);
+        }
+
+        void MHoover(object s, EventArgs e)
+        {
+            var newSubItem = new ToolStripMenuItem();
+            newSubItem.Name = stripCounter.ToString();
+            newSubItem.Text = stripCounter.ToString();
+            newSubItem.Width = 10;
+            var sender = s as ToolStripMenuItem;
+            stripCounter++;
+            newSubItem.MouseHover += MHoover;
+            sender.DropDownItems.Add(newSubItem);
+        }
+
+    }
+}
